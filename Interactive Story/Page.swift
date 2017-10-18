af26e8530df8a619cbe4205a70b6d6c66351cc48
Fidele <crescens.kob@outlook.fr>
2017-10-18 23:10:24 +0800

    Creating helper methods

--- Interactive Story/Page.swift
+++ Interactive Story/Page.swift
@@ -21,6 +21,23 @@ class Page {
     }
 }
 
+extension Page {
+    
+    func addChoiceWith(title: String, story: Story) -> Page {
+        let page = Page(story: story)
+        return addChoiceWith(title: title, page: page)
+    }
+    
+    func addChoiceWith(title: String, page: Page) -> Page {
+        switch (firstChoice, secondChoice) {
+        case (.some, .some): return self
+        case (.none, .none), (.none, .some): firstChoice = (title, page)
+        case (.some, .none): secondChoice = (title, page)
+        }
+        return page
+    }
+}
+
 
 
 

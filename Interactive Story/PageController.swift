0289705a546806ff553331654f268d27f3d55f1c
Fidele <crescens.kob@outlook.fr>
2017-10-19 19:31:09 +0800

    Page Controller

---
+++ Interactive Story/PageController.swift
@@ -0,0 +1,34 @@
+//
+//  PageController.swift
+//  Interactive Story
+//
+//  Created by Mac on 19/10/2017.
+//  Copyright © 2017 Smart Soft. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+
+    var page: Page?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page){
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

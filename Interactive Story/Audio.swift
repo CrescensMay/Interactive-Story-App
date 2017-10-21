3d8f6943057564b9a52f96e54f5c2f5dba6a9303
Fidele <crescens.kob@outlook.fr>
2017-10-21 19:55:48 +0800

    Added sounds files and created urls for sound files

---
+++ Interactive Story/Audio.swift
@@ -0,0 +1,28 @@
+//
+//  Audio.swift
+//  Interactive Story
+//
+//  Created by Mac on 21/10/2017.
+//  Copyright © 2017 Smart Soft. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+extension Story {
+    var soundEffectName: String {
+        switch self {
+        case .droid, .home: return "HappyEnding"
+        case .monster: return "Ominous"
+        default: return "PageTurn"
+        }
+    }
+    var soundEffectURL: URL {
+        let path = Bundle.main.path(forResource: soundEffectName, ofType: "wav")!
+        return URL(fileURLWithPath: path)
+    }
+}
+class SoundEffectsPlayer {
+    var sound: SystemSoundID = 0
+    
+}
